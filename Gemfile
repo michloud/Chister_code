31a63cbd2b04a5c8c8f96c288835da10112cb2c0
michloud <chinoah96@gmail.com>
2019-09-16 18:38:02 +0000

    add geocoder gem and API key

--- Gemfile
+++ Gemfile
@@ -74,3 +74,10 @@ gem 'will_paginate-bootstrap'
 gem 'simple_form'
 gem "font-awesome-rails"
 gem 'devise'
+
+gem 'geocoder'
+gem 'figaro'
+
+
+
+
--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,8 @@
 class Place < ApplicationRecord
   belongs_to :user
+  geocoded_by :address
+  after_validation :geocode
+  
   validates :address, :description, presence: true
   validates :name, presence: true, length: {minimum: 4}
 end 
