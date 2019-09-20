c3f538d7beedc27765164ee323f3372cffebfc76
michloud <chinoah96@gmail.com>
2019-09-20 04:29:49 +0000

    add columns and indexes to photos tables

---
+++ db/migrate/20190920041135_alter_photos_add_user_id_column.rb
@@ -0,0 +1,8 @@
+class AlterPhotosAddUserIdColumn < ActiveRecord::Migration[5.2]
+  def change
+
+    add_column :photos, :user_id, :integer
+    add_index :photos, [:user_id, :place_id]
+    add_index :photos, :place_id
+  end
+end
