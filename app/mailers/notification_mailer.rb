e65bd171c252752caca03121cffe0be907137064
michloud <chinoah96@gmail.com>
2019-09-23 18:44:45 +0000

    configure email to go to user,include place in email subject line,include linkt to place in body of email

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -3,9 +3,12 @@ class NotificationMailer < ApplicationMailer
 
  
 
-  def comment_added
-    mail(to: "chinoah96@gmail.com",
-      subject: "A comment has been added to your place")
+  def comment_added(comment)
+    @place = comment.place
+    @place_owner = @place.user
+
+    mail(to: @place_owner.email,
+      subject: "A comment has been added to #{@place.name}")
 
 
   end
